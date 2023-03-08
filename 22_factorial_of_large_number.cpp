331c06bba4366d34a548a1e2df9f16286514cfe6
sangwan14 <68826136+sangwan14@users.noreply.github.com>
2023-03-08 20:07:49 +0530

    Create 22_factorial_of_large_number.cpp

---
+++ 22_factorial_of_large_number.cpp
@@ -0,0 +1,18 @@
+vector<int> factorial(int N){
+        // code here
+        vector<int> res = {1};
+        for(int i=2; i<=N; i++){
+            int carry = 0;
+            for(int j=0; j<res.size(); j++){
+                int val = res[j]*i+carry;
+                res[j] = val%10;
+                carry = val/10;
+            }
+            while(carry != 0){
+                res.push_back(carry%10);
+                carry /= 10;
+            }
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
